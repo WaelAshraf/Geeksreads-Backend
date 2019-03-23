4a6941979122b2ae4ef803537bb6d11851937dc4
MohamedAshraf1913 <48304984+MohamedAshraf1913@users.noreply.github.com>
2019-03-23 13:48:39 +0200

    Update Authontication.test.js

--- Software Engineering/Test/Authontication.test.js
+++ Software Engineering/Test/Authontication.test.js
@@ -30,31 +30,31 @@ it("Invalid Name Sign Up",  (done) => {
         })
     .expect(400,done);
 });
-// it("Unverified User",  (done) => {
-//     request(app)
-//      .post('/api/Auth/')
-//      .send({
-//              "UserEmail": "samersosta@gmail.com",
-//              "UserPassword": "123456"
-//         })
-//     .expect(401,done);
-// });
-// it("Send Verification Email",  (done) => {
-//     request(app)
-//      .post('/api/Users/')
-//      .send({
-//              "UserName": "Saad",
-//              "UserEmail": "samersosta@gmail.com",
-//              "UserPassword": "123456"
-//         })
-//     .expect(200,done);
-// })
-// it("Login Succefully",  (done) => {
-//     request(app)
-//      .post('/api/Auth/')
-//      .send({
-//              "UserEmail": "samersosta@gmail.com",
-//              "UserPassword": "123456"
-//         })
-//     .expect(200,done);
-// })
+ it("Unverified User",  (done) => {
+     request(app)
+      .post('/api/Auth/')
+      .send({
+              "UserEmail": "samersosta@hotmail.com",
+              "UserPassword": "123456"
+         })
+     .expect(401,done);
+ });
+ it("Send Verification Email",  (done) => {
+     request(app)
+      .post('/api/Users/')
+      .send({
+              "UserName": "Saad",
+              "UserEmail": "samersosta2@Outlook.com",
+              "UserPassword": "123456"
+         })
+     .expect(200,done);
+ })
+ it("Login Succefully",  (done) => {
+     request(app)
+      .post('/api/Auth/')
+      .send({
+              "UserEmail": "samersosta@gmail.com",
+              "UserPassword": "123456"
+         })
+     .expect(200,done);
+})
