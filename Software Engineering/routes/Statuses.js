8caf0d5b2b54636f541e52dc7762b9d849ecf35f
MohamedAshraf1913 <48304984+MohamedAshraf1913@users.noreply.github.com>
2019-04-06 22:00:07 +0200

    Update Statuses.js

--- Software Engineering/routes/Statuses.js
+++ Software Engineering/routes/Statuses.js
@@ -4,7 +4,7 @@ const {Status,validate} = require('../models/Statuses');
 const {User} = require('../models/User');
 const auth = require('../middleware/auth');
 const Joi = require('joi');
-const auth = require('../middleware/auth');
+//const auth = require('../middleware/auth');
 const router = express.Router();
 
 
@@ -12,20 +12,20 @@ const router = express.Router();
 /**
  * @api{Post} /user_status/ Update user status
  * @apiVersion 0.0.0
- * @apiName UpdateUserStatuses 
+ * @apiName UpdateUserStatuses
  * @apiGroup Status
- * 
+ *
  * @apiParam {string} StatusID status id
  * @apiParam {string} UserID User id
  * @apiparam {String} StatusBody the body of this status
  * @apiparam {datePicker} StatusDate the date when the status was written
- * @apiparam {string} CommentId comment id <code>(optional)</code> 
- * @apiparam {string} ReviewId <code>(optional)</code> 
+ * @apiparam {string} CommentId comment id <code>(optional)</code>
+ * @apiparam {string} ReviewId <code>(optional)</code>
  *
  *  @apiSuccess {boolen} UpdateSucc  if the update happend successfully or not
- *   
+ *
  * @apiSuccessExample  Expected Data on Success
- * { 
+ * {
  *  "UpdateSucc": true
  * }
  * @apiError User-Not-Found The <code>User</code> was not found
@@ -34,14 +34,14 @@ const router = express.Router();
 
 router.post("/",(req,res)=>
 {
- 
+
  if (req.body.length == 0)
  {
     return res.status(400).send("No parameters was sent")
  }
 
 
- var  newStatus = new Status( 
+ var  newStatus = new Status(
    {
    "StatusId":req.body.StatusId,
     "UserId":req.body.UserId,
@@ -53,52 +53,52 @@ router.post("/",(req,res)=>
 
   const {error} = validate(newStatus.body);
   if (error) return res.status(400).send(error.details[0].message);
-  
-   
+
+
    Status.findOne({'StatusId': newStatus.StatusId},(err,doc)=>
    {
     if(doc)
     {
    return res.status(400).send("already exist");
     }
-    newStatus.save()   
+    newStatus.save()
     res.status(200).send(
         {
             UpdateSucc : true
         }
-    );   
+    );
    });
 
-  
-});    
+
+});
   /**
  * @api{Get} /user_status/show Get User Status
  * @apiVersion 0.0.0
- * @apiName GetUserStatuses 
+ * @apiName GetUserStatuses
  * @apiGroup Status
  * @apiHeader {String} x-auth-token Authentication token
  *
  * @apiSuccess {string} StatusId status id
- * @apiSuccess {string} UserId User id the user who is to see the status 
+ * @apiSuccess {string} UserId User id the user who is to see the status
  * @apiSuccess {datePicker} StatusDate the date when the status was written
- * @apiSuccess {string} CommentId comment id if the type is comment <code>(optional)</code> 
+ * @apiSuccess {string} CommentId comment id if the type is comment <code>(optional)</code>
  * @apiSuccess {string} ReviewId  review Id  alawys exisit weather the type is comment or review
  * @apiSuccess {string} MakerId the id of the user who made the status
- * @apiSuccess {string} Type  Wheather  it is Comment or Review 
+ * @apiSuccess {string} Type  Wheather  it is Comment or Review
  * @apiSuccessExample  Expected Data on Success
  * {
  *
- * type : Review 
+ * type : Review
  * StatusId : "82978363763"
  * MakerId : "shjfhghdsg"
  * UserId : "82sdfd8363763"
  * ReviewId : "82gf8363763"
- * 
- * }, 
+ *
+ * },
  * {
- * 
+ *
  * type : Comment
- * CommentId : "hisadsfjhdld"  
+ * CommentId : "hisadsfjhdld"
  * StatusId : "82978363763"
  * MakerId : "shjfhghdsg"
  * UserId : "82sdfd8363763"
@@ -110,7 +110,7 @@ router.post("/",(req,res)=>
  *  {
  *    "ReturnMsg":"No statuses were found"
  *  }
- * 
+ *
  * @apiErrorExample {json} Invalidtoken-Response:
  *     HTTP/1.1 400
  *   {
@@ -133,7 +133,7 @@ router.get("/show",auth ,(req,res)=>
      {
         return  res.status(400).send("Bad request no UserID  Id is there");
     }
- 
+
       if (req.body.UserId.length == 0)
      {
        return  res.status(400).send("Bad request no Satatus Id is there");
@@ -151,22 +151,22 @@ router.get("/show",auth ,(req,res)=>
     {
    return res.status(404).send("No statuses were found");
     }
-      
+
     res.status(200).send(
         doc
     )
    }
 )
-    
+
 
 });/**
-* @api{Post} /user_status/delete Delete User Status 
+* @api{Post} /user_status/delete Delete User Status
 * @apiVersion 0.0.0
-* @apiName DeleteStatus  
+* @apiName DeleteStatus
 * @apiGroup Status
-* 
+*
 * @apiParam {string} StatusId Status id
-* 
+*
 * @apiSuccess {boolen} DeleteStatusSuc  if the delete happend successfully or not
 * @apiSuccessExample  Expected Data on Success
 * {
@@ -180,7 +180,7 @@ router.post("/delete",(req,res)=>
      {
         return  res.status(400).send("Bad request no statusID  Id is there");
     }
- 
+
       if (req.body.StatusId.length == 0)
      {
        return  res.status(400).send("Bad request no Satatus Id is there");
@@ -198,33 +198,33 @@ router.post("/delete",(req,res)=>
     {
    return res.status(404).send("Statuses Not found for this User");
     }
-      
+
     res.status(200).send(
-   { 
+   {
        "DeleteSTatusSuc" : true
-   
+
     }
     )
    }
 )
-    
+
 
 });
 
 function CreatStatuses( FollowerId ,ReviewId ,StatusDate, CommentId, StatusBody )
 {
-  var  newStatus = new Status( 
-{ 
+  var  newStatus = new Status(
+{
   "UserId":FollowerId,
   "ReviewId":ReviewId,
   "CommentId":CommentId,
   "StatusDate":StatusDate
  });
  newStatus.StatusId=newStatus._id;
-  
+
  const {error} = validate(newStatus.body);
  if (error) return console.log(error.details[0].message);
- 
+
   if (StatusBody)
   {
     newStatus.body=StatusBody;
@@ -232,8 +232,8 @@ function CreatStatuses( FollowerId ,ReviewId ,StatusDate, CommentId, StatusBody 
 }
 
 function CreatStatusBody(UserId,CommentId,ReviewId,bookId)
-{  
-User.findOne({'UserId': UserId},(err,doc)=>{ 
+{
+User.findOne({'UserId': UserId},(err,doc)=>{
   if  (!doc)
 {
   return console.log (" there no scuch a user ")
@@ -245,15 +245,15 @@ if (doc)
   {
   return newStatus.body= doc.UserName + " commented on " +ReviewId;
   }
-  
+
   else if (bookId){
-    
-    
+
+
    return newStatus.body = doc.UserName + " reviewed on " +ReviewId;
   }
   else return null
 }
-    });   
+    });
   }
 
 
